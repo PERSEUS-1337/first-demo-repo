c55f27c026df046ea30b7fd5b0957152dfdeba62
PERSEUS-1337 <abramillano@up.edu.ph>
2022-02-18 21:44:15 +0800

    SMH, another update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fs = require('fs');
+// const fs = require('fs');
 
 const {google} = require("googleapis");
 const bodyParser = require('body-parser');
@@ -31,21 +31,21 @@ function sampleDataLoop(arr) {
     return arr;
 }
 
-function jsonCreator () {
-    authObject = {
-        type: process.env.TYPE,
-        project_id: process.env.PROJECT_ID,
-        private_key_id: process.env.PRIVATE_KEY_ID,
-        private_key: process.env.PRIVATE_KEY,
-        client_email: process.env.CLIENT_EMAIL,
-        client_id: process.env.CLIENT_ID,
-        auth_uri: process.env.AUTH_URI,
-        token_uri: process.env.TOKEN_URI,
-        auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
-        client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
-    }
-    return authObject;
-}
+// function jsonCreator () {
+//     authObject = {
+//         type: process.env.TYPE,
+//         project_id: process.env.PROJECT_ID,
+//         private_key_id: process.env.PRIVATE_KEY_ID,
+//         private_key: process.env.PRIVATE_KEY,
+//         client_email: process.env.CLIENT_EMAIL,
+//         client_id: process.env.CLIENT_ID,
+//         auth_uri: process.env.AUTH_URI,
+//         token_uri: process.env.TOKEN_URI,
+//         auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+//         client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+//     }
+//     return authObject;
+// }
 
 // middleware functions
 function logger(req, res, next) {
